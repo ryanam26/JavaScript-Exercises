4cfd72bf7674b98675a207c5998d51a0b708091c
Ryan Morrison <ryanam26@yahoo.com>
2020-03-25 11:24:15 -0400

    working on objects

---
+++ objects.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  
+  let isActive = spaceship["Active Mission"]
+  
+  console.log(isActive)
