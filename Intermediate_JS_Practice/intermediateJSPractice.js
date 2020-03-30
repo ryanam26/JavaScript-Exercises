b70d75b9e1674eb0d1d2f5e8e1771e59808ead71
Ryan Morrison <ryanam26@yahoo.com>
2020-03-30 12:59:46 -0400

    do not use .forEach() method

--- Intermediate_JS_Practice/intermediateJSPractice.js
+++ Intermediate_JS_Practice/intermediateJSPractice.js
@@ -1,5 +1,4 @@
-// reverse an array without using a built in function
-
+// reverse an array without using a built in function, Could use .reverse() method
 const reverseArray = (arr) => {
   
     let reverseArr = [];
@@ -13,3 +12,16 @@ const reverseArray = (arr) => {
   const sentence = ['sense.','make', 'all', 'will', 'This'];
   
   console.log(reverseArray(sentence)) 
+
+
+
+  // build function without using .forEach method
+  const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+  let greetAliens = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      console.log("Oh powerful " + arr[i] + ", we humans offer our unconditional surrender!")
+    }
+  };
+  
+  greetAliens(aliens)
