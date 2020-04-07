c5d55df5bbdb0fbcccb86061e12242d4399e6341
Ryan Morrison <ryanam26@yahoo.com>
2020-04-06 22:10:39 -0400

    randomize doors

--- choreBot/script.js
+++ choreBot/script.js
@@ -1,20 +1,47 @@
-const doorImage1 = document.getElementById("door1");
+
+
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let openDoor1 
+let openDoor2 
+let openDoor3
 
+const numClosedDoors = 3;
+
+const doorImage1 = document.getElementById("door1");
 const doorImage2 = document.getElementById("door2");
 const doorImage3 = document.getElementById("door3");
 
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors );
+    if ( choreDoor === 0 ) {
+        openDoor1 = botDoorPath
+        openDoor2 = beachDoorPath
+        openDoor3 = spaceDoorPath
+    } else if ( choreDoor === 1) {
+        openDoor3 = botDoorPath
+        openDoor1 = beachDoorPath
+        openDoor2 = spaceDoorPath
+    } else {
+        openDoor2 = botDoorPath
+        openDoor3 = beachDoorPath
+        openDoor1 = spaceDoorPath
+    }
+}
+
+
 
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 };
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 };
+
+randomChoreDoorGenerator()
