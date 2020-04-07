e70d90eae62fe9613a7813101d538b3cdd796c41
Ryan Morrison <ryanam26@yahoo.com>
2020-04-06 20:51:50 -0400

    Style added

--- choreBot/script.js
+++ choreBot/script.js
@@ -1,1 +1,20 @@
-let doorImage1 = document.getElementById(door1);
+const doorImage1 = document.getElementById("door1");
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+const doorImage2 = document.getElementById("door2");
+const doorImage3 = document.getElementById("door3");
+
+
+doorImage1.onclick = () => {
+    doorImage1.src = botDoorPath;
+};
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+};
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
+};
