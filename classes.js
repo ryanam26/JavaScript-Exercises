ddf9dd9c04e0bd0cd866411eab0726e6887bbb3d
Ryan Morrison <ryanam26@yahoo.com>
2020-04-14 13:16:21 -0400

    Finished library class

--- classes.js
+++ classes.js
@@ -1,5 +1,94 @@
-// Build a library.  Practice Classes and inheritance
-
 class Media {
+    constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+	 this._ratings = [];
+    }
 
-};
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+    
+    get ratings() {
+        return this._ratings;
+    }
+    
+    set isCheckedOut(newisCheckedOut) {
+        this._isCheckedOut = newisCheckedOut;
+    }
+    
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+    
+    addRating(ratings){
+        this._ratings.push(ratings);
+    }
+    getAverageRating() {
+        let ratingsAverage = this.ratings.reduce((acc, rating) => acc + rating);
+        return ratingsAverage / this.ratings.length
+    }  
+}
+
+
+class Book extends Media {
+    constructor (author,title,pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+  }
+  get author (){
+      return this._author;
+  }
+  get pages (){
+      return this._pages;
+  }
+}
+
+class Movie extends Media{
+	constructor(director,title,runTime){
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+    get director (){
+        return this._director;
+    }
+    get runTime (){
+        return this._runTime;
+	}
+}
+
+
+// New book Instance
+const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut)
+
+historyOfEverything.addRating(4)
+historyOfEverything.addRating(5)
+historyOfEverything.addRating(5)
+
+historyOfEverything.getAverageRating()
+
+console.log(historyOfEverything.getAverageRating())
+
+// New Movie instance
+const speed = new Movie("Jan de Bont", "Speed", 116);
+
+speed.toggleCheckOutStatus()
+console.log(speed.isCheckedOut)
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+console.log(speed.getAverageRating())
+
+
