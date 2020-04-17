ee4b307f3314746da839c38e6921c9a89b98ddbb
Ryan Morrison <ryanam26@yahoo.com>
2020-04-17 12:13:07 -0400

    POst request with asyc await

--- requestsWithAsyncAwait.js
+++ requestsWithAsyncAwait.js
@@ -1,1 +1,32 @@
-// get requests async...await
+// GET requests async...await
+
+const getData = async () => {
+    try {
+      const response = await fetch("https://api-to-call.com/endpoint");
+      if (response.ok) {
+        const jsonResponse = await response.json();
+            return jsonResponse
+      }
+      throw new Error("Request failed!")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // POST request async...await
+  
+  const getData = async () => {
+    try {
+      const response = await fetch('https://api-to-call.com/endpoint', {
+        method: 'POST',
+        body: JSON.stringify({id: 200})
+      })
+      if(response.ok){
+        const jsonResponse = await response.json();
+        return jsonResponse;
+      }
+      throw new Error('Request failed!');
+    } catch(error) {
+      console.log(error);
+    }
+  }
