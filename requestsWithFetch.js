d3e9fbfac73209ea13276a92237d426062be71e1
Ryan Morrison <ryanam26@yahoo.com>
2020-04-17 10:29:00 -0400

    GET and POST with Fetch()

--- requestsWithFetch.js
+++ requestsWithFetch.js
@@ -1,2 +1,27 @@
 // Get requests with fetch()
 
+fetch("https://api-to-call.com/endpoint").then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Request failed!");
+}, networkError => {console.log(networkError.message)
+}).then(jsonResponse => {
+  return jsonResponse;
+})
+
+
+// POST with fetch()
+
+fetch("https://api-to-call.com/endpoint", {
+  method: "POST",
+  body: JSON.stringify({id:'200'})
+}).then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Request failed!");
+}, networkError => console.log(networkError.message)
+).then(jsonResponse => {
+  return jsonResponse;
+});
