6872dec06c2e2e0b37256ce706a6fa0716d4f0f8
Ryan Morrison <ryanam26@yahoo.com>
2020-05-12 14:08:59 -0400

    express app

---
+++ expressApp/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
