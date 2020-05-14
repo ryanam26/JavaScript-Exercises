f3aae8107fe6710b277679b0dd3f7fae2442376c
Ryan Morrison <ryanam26@yahoo.com>
2020-05-14 13:25:51 -0400

    completed

--- ExpressAuth/index.js
+++ ExpressAuth/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 
@@ -9,9 +10,41 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static('public'))
 
-// @TODO add auth middleware
+// auth middleware
+app.use(require('express-session')({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false
+  }))
+  
+  const { ExpressOIDC } = require('@okta/oidc-middleware')
+  const oidc = new ExpressOIDC({
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+    scope: 'openid profile'
+  })
+  
+app.use(oidc.router)
+
+
 // @TODO add registration page
+app.use('/register', require('./routes/register'))
+
 // @TODO add logout route
+app.get('/logout', (req, res) => {
+    if (req.userContext) {
+      const idToken = req.userContext.tokens.id_token
+      const to = encodeURI(process.env.HOST_URL)
+      const params = `id_token_hint=${idToken}&post_logout_redirect_uri=${to}`
+      req.logout()
+      res.redirect(`${process.env.OKTA_ORG_URL}/oauth2/default/v1/logout?${params}`)
+    } else {
+      res.redirect('/')
+    }
+  })
+
 
 app.use('/', require('./routes/index'))
 
